49699ff18b3b10d38b5ce5760cbb456e1973a147
anilates97 <anilates.97@gmail.com>
2023-11-02 05:34:46 +0300

    added weather icons

--- src/WeatherItem.js
+++ src/WeatherItem.js
@@ -1,6 +1,22 @@
 import { useWeather } from "./context/WeatherContext";
 import { CSSTransition } from "react-transition-group";
-import iconWeather from "../src/img/clear-sky.png";
+import clearSky from "../src/img/clear-sky.png";
+import partlyCloudy from "../src/img/partly-cloudy.png";
+import cloudy from "../src/img/clouds.png";
+import overcast from "../src/img/overcast.png";
+import mist from "../src/img/mist.png";
+import patchyRain from "../src/img/mist.png";
+import patchySnow from "../src/img/snow.png";
+import sleet from "../src/img/sleet.png";
+import drizzle from "../src/img/drizzle.png";
+import thunderStorm from "../src/img/thunderstorm.png";
+import blowSnowy from "../src/img/blowSnowy.png";
+import blizzard from "../src/img/blizzard.png";
+import fog from "../src/img/fog.png";
+import lightRain from "../src/img/light-rain.png";
+import heavyRain from "../src/img/heavy-rain.png";
+import thunderRain from "../src/img/thunderRain.png";
+import moderateRain from "../src/img/moderateRain.png";
 
 function WeatherItem() {
   const { weather, showContent, status } = useWeather();
@@ -9,7 +25,158 @@ function WeatherItem() {
   let img;
   if (status === "loaded") {
     if (current.condition.text.toLowerCase() === "clear") {
-      img = <img src={iconWeather} alt="icon" className="conditionIcon" />;
+      img = <img src={clearSky} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "sunny") {
+      img = <img src={clearSky} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "partly cloudy") {
+      img = <img src={partlyCloudy} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "cloudy") {
+      img = <img src={cloudy} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "overcast") {
+      img = <img src={overcast} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "mist") {
+      img = <img src={mist} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Patchy rain possible") {
+      img = <img src={patchyRain} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Patchy snow possible") {
+      img = <img src={patchySnow} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Patchy sleet possible") {
+      img = <img src={sleet} alt="icon" className="conditionIcon" />;
+    }
+    if (
+      current.condition.text.toLowerCase() ===
+      "Patchy freezing drizzle possible"
+    ) {
+      img = <img src={sleet} alt="icon" className="conditionIcon" />;
+    }
+    if (
+      current.condition.text.toLowerCase() === "Thundery outbreaks possible"
+    ) {
+      img = <img src={thunderStorm} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Blowing snow") {
+      img = <img src={blowSnowy} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Blizzard") {
+      img = <img src={blizzard} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "fog") {
+      img = <img src={fog} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Freezing fog") {
+      img = <img src={fog} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Patchy light drizzle") {
+      img = <img src={drizzle} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "light drizzle") {
+      img = <img src={drizzle} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Freezing drizzle") {
+      img = <img src={drizzle} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Heavy freezing drizzle") {
+      img = <img src={drizzle} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Patchy light rain") {
+      img = <img src={lightRain} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Light rain") {
+      img = <img src={lightRain} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Moderate rain at times") {
+      img = <img src={moderateRain} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Heavy rain") {
+      img = <img src={heavyRain} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Light freezing rain") {
+      img = <img src={heavyRain} alt="icon" className="conditionIcon" />;
+    }
+    if (
+      current.condition.text.toLowerCase() === "Moderate or heavy freezing rain"
+    ) {
+      img = <img src={heavyRain} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Light sleet") {
+      img = <img src={sleet} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Moderate or heavy sleet") {
+      img = <img src={sleet} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Patchy light snow") {
+      img = <img src={patchySnow} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Light snow") {
+      img = <img src={patchySnow} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Patchy moderate snow") {
+      img = <img src={patchySnow} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Patchy heavy snow") {
+      img = <img src={patchySnow} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Heavy snow") {
+      img = <img src={blowSnowy} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Ice pellets") {
+      img = <img src={blowSnowy} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Light rain shower") {
+      img = <img src={lightRain} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Torrential rain shower") {
+      img = <img src={lightRain} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Light sleet showers") {
+      img = <img src={sleet} alt="icon" className="conditionIcon" />;
+    }
+    if (
+      current.condition.text.toLowerCase() === "Moderate or heavy sleet showers"
+    ) {
+      img = <img src={sleet} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Light snow showers") {
+      img = <img src={patchySnow} alt="icon" className="conditionIcon" />;
+    }
+    if (current.condition.text.toLowerCase() === "Light snow showers") {
+      img = <img src={patchySnow} alt="icon" className="conditionIcon" />;
+    }
+    if (
+      current.condition.text.toLowerCase() ===
+      "Moderate or heavy showers of ice pellets"
+    ) {
+      img = <img src={blowSnowy} alt="icon" className="conditionIcon" />;
+    }
+    if (
+      current.condition.text.toLowerCase() === "Patchy light rain with thunder"
+    ) {
+      img = <img src={thunderRain} alt="icon" className="conditionIcon" />;
+    }
+    if (
+      current.condition.text.toLowerCase() ===
+      "Moderate or heavy rain with thunder"
+    ) {
+      img = <img src={thunderRain} alt="icon" className="conditionIcon" />;
+    }
+    if (
+      current.condition.text.toLowerCase() === "Patchy light snow with thunder"
+    ) {
+      img = <img src={thunderStorm} alt="icon" className="conditionIcon" />;
+    }
+    if (
+      current.condition.text.toLowerCase() ===
+      "Moderate or heavy snow with thunder"
+    ) {
+      img = <img src={blowSnowy} alt="icon" className="conditionIcon" />;
     }
   }
 
